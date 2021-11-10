ff8ab16cb8f86a34ccb110b011f101fd42f3a979
ali <ali.zaferany79@gmail.com>
2021-11-11 00:34:13 +0330

    fea : add models

--- OpenTelemetry-Hello-World/Models/AppDbContext.cs
+++ OpenTelemetry-Hello-World/Models/AppDbContext.cs
@@ -6,5 +6,7 @@ namespace OpenTelemetry
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         { }
+
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }              
     }
 }
