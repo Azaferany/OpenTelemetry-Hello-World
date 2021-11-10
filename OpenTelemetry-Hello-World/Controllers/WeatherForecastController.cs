8738e951c3e1f906e8753ccb2484031a5a3ac7c7
ali <ali.zaferany79@gmail.com>
2021-11-11 00:34:50 +0330

    fea : Add Controller

---
+++ OpenTelemetry-Hello-World/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace OpenTelemetry
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly AppDbContext _context;
+        private static readonly ActivitySource ActivitySource = new(nameof(WeatherForecastController));
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, 
+            AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+
+            using (ActivitySource.StartActivity("Process Delay").SetParentId(Activity.Current.Id).Start())
+            {
+                _logger.LogInformation("Wait For Some Delay");
+                await Task.Delay(5000);
+            }
+
+            return await _context.WeatherForecasts.ToArrayAsync();
+        }
+    }
+}
